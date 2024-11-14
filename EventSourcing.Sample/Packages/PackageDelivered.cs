7a8e7afe18483e616dbd5bbd3da627620a10da00
Edvard Jinder <edvard.jinder@intromedia.se>
2024-11-14 12:30:23 +0100

    Added license and readme

---
+++ EventSourcing.Sample/Packages/PackageDelivered.cs
@@ -0,0 +1,8 @@
+﻿internal sealed class PackageDelivered : IEvent
+{
+    public Guid EventId { get; set; } = Guid.NewGuid();
+    public required Guid PackageId { get; set; }
+    public required DateTime Timestamp { get; set; }
+    public required int Version { get; set; }
+}
+
